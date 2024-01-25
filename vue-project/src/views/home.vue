6b089b5117537ddee162c2c8948cb643d0609d52
IceTeaPeche <ethan.ledouble@epitech.digital>
2024-01-25 22:46:57 +0100

    [ADD] navbar home

--- vue-project/src/views/home.vue
+++ vue-project/src/views/home.vue
@@ -1,5 +1,37 @@
 <template>
-    <H1 class="underline ">home</H1>
+
+    <header>
+
+            <nav class="flex items-center justify-between ">
+
+                <div class="ml-7">
+                    <div>
+                        <img height="60px" width="60px" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Ellipse 3.png" alt="">
+                    </div>
+                </div>
+
+                <div class=" ml-4">
+                    <div>
+                        <img height="80" width="80" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/logofinal.svg" alt="">
+                    </div>
+                </div>
+
+                <div class="flex mr-7">
+                    <div class="mr-4">
+                        <img src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Vector.png" alt="">
+                    </div>
+
+                    <div>
+                        <img src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Vectorpara.png" alt="">
+                    </div>
+                </div>
+
+            </nav>
+     <div class="h-0.5 bg-gray-500 mr-3 ml-3"></div>
+     </header>
+
+
+
 </template>
 
 
