e87952ec598973d223573c3576c788156300ef91
IceTeaPeche <ethan.ledouble@epitech.digital>
2024-01-26 11:23:40 +0100

    [ADD] Home

--- vue-project/src/views/home.vue
+++ vue-project/src/views/home.vue
@@ -6,18 +6,18 @@
 
                 <div class="ml-7">
                     <div>
-                        <img height="60px" width="60px" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Ellipse 3.png" alt="">
+                        <img height="50px" width="50px" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Ellipse 3.png" alt="">
                     </div>
                 </div>
 
-                <div class=" ml-4">
+                <div class=" ml-5">
                     <div>
                         <img height="80" width="80" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/logofinal.svg" alt="">
                     </div>
                 </div>
 
                 <div class="flex mr-7">
-                    <div class="mr-4">
+                    <div class="mr-4 move-up">
                         <img src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Vector.png" alt="">
                     </div>
 
@@ -27,8 +27,56 @@
                 </div>
 
             </nav>
-     <div class="h-0.5 bg-gray-500 mr-3 ml-3"></div>
+     <div class="barre bg-gray-500 mr-3 ml-3 mt-0 move-upe"></div>
      </header>
+
+     <body>
+        
+        <section class="">
+
+            <div class="barre bg-gray-500 "></div>
+
+            <div class="ml-4 flex"> 
+                
+                <img width="35" height="35" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Ellipse 3.png" alt="">
+
+                <h1 class="text-white mt-3.5 ml-1 font-bold text-xl">Marioppp</h1>
+            
+            </div>
+
+            <div class="box-width ml-10 mt-0" > 
+
+                <h2 class="text-white box-width break-words ml-3.5 mt-0  ">sq,dklqjskldjqksndkonzojanfjoeznfjodnsfklsdfkjsiofsndfklsdnfsidofnsoklndfklsndkflsndfoinsdkflnsdlfkqpqqp</h2>
+
+            </div>
+
+            <div class="flex mt-0">
+
+               <div class="w-20 h-7 border border-white rounded-3xl ml-12 flex">
+            <img class="rounded-9xl mb-1.5 ml-2 h-5 w-5 transform -translate-y-1" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/coeur.svg" alt="">
+            <h2 class="text-white rounded-3xl margin-top ml-3 mr-2 text-sm">19k</h2>
+        </div>
+        <div class="w-20 h-7 border border-white rounded-3xl ml-3 flex">
+            <img class="rounded-9xl mb-1.5 ml-2 h-5 w-5 transform -translate-y-1" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/comment.svg" alt="">
+            <h2 class="text-white rounded-3xl margin-top ml-3 mr-2 text-sm">19k</h2>
+        </div>
+        <div class="w-20 h-7 border border-white rounded-3xl ml-3 flex">
+            <img class="rounded-9xl mb-1.5 ml-2 h-5 w-5 transform -translate-y-1" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/partage.svg" alt="">
+            <h2 class="text-white rounded-3xl margin-top ml-3 mr-2 text-sm">19k</h2>
+        </div>
+        <div class="w-20 h-7 border border-white rounded-3xl ml-8 flex">
+            <img class="rounded-9xl mb-1.5 ml-2 h-5 w-5 transform -translate-y-1" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/favoris.svg" alt="">
+            <h2 class="text-white rounded-3xl margin-top ml-2 mr-2 text-sm">19k</h2>
+        </div>
+             
+
+
+            </div>
+
+        </section>
+
+
+     </body>
 
 
 
@@ -38,4 +86,27 @@
 <script>
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.move-up {
+    transform: translateY(-3px);
+}
+
+.move-upe {
+    transform: translateY(-10px);
+}
+
+.barre{
+    height: 1px;
+}
+
+.box-width {
+    width: 350px;
+    
+}
+
+.margin-top {
+    margin-top: 2px;
+}
+
+</style>
