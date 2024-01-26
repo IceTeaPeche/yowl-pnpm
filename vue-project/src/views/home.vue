054a0bc337b381dac4a67ee4985cdde06b9daea9
Mizeeqx <146079284+Mizeeqx@users.noreply.github.com>
2024-01-26 11:22:41 +0100

    [add] login

--- vue-project/src/views/home.vue
+++ vue-project/src/views/home.vue
@@ -6,23 +6,23 @@
 
                 <div class="ml-7">
                     <div>
-                        <img height="60px" width="60px" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Ellipse 3.png" alt="">
+                        <img height="60px" width="60px" src="" alt="">
                     </div>
                 </div>
 
                 <div class=" ml-4">
                     <div>
-                        <img height="80" width="80" src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/logofinal.svg" alt="">
+                        <img height="80" width="80" src="" alt="">
                     </div>
                 </div>
 
                 <div class="flex mr-7">
                     <div class="mr-4">
-                        <img src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Vector.png" alt="">
+                        <img src="" alt="">
                     </div>
 
                     <div>
-                        <img src="/Users/ethanledouble/github/yowl-pnpm/vue-project/src/assets/Vectorpara.png" alt="">
+                        <img src="" alt="">
                     </div>
                 </div>
 
