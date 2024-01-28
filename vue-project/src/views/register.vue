a42cf748f5cb07dce9a232112376556f355f6e57
IceTeaPeche <ethan.ledouble@epitech.digital>
2024-01-28 19:01:31 +0100

    [DEL] Strapi v4.19

--- vue-project/src/views/register.vue
+++ vue-project/src/views/register.vue
@@ -6,21 +6,22 @@
 
         <form class="flex flex-col w-full max-w-xs gap-7 mt-[275px]">
 
-            <input type="text" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2 z-1" placeholder="Pseudo"
+            <input type="text" id="username" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2 z-1" placeholder="Username" 
                 required>
 
-            <input type="text" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2" placeholder="Birthdate MM/DD/YYYY"
-                    required>
+            <input type="text" id="birthday" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2" placeholder="Birthdate MM/DD/YYYY">
 
-            <input type="text" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2" placeholder="Mail"
+            <input type="text" id="email" class="px-2.5 border border-gray-300 rounded-2xl text-white pt-2 pb-2" placeholder="Mail" v-model="email" @input="validateEmail"
                     required>
+           
 
 
 
             <div class="relative flex mb-[-22px]">
 
-                <input class="px-2.5 border border-gray-300 rounded-2xl text-white flex-grow pt-2 pb-2" id="password"
+                <input class="px-2.5 border border-gray-300 rounded-2xl text-white flex-grow pt-2 pb-2" id="password" v-model="password" @input="validatePassword"
                     type="password" placeholder="Password" for required>
+                
 
             
 
@@ -28,7 +29,7 @@
             </div>
 
 
-         <div class="flex">
+         <div class="flex transform translate-y-2.5">
         <input class="px-2.5 text-white flex-grow pt-2 pb-2 text-[10px] pr-0 "
             placeholder="do you accept cookies ?" >
 
@@ -46,6 +47,8 @@
         </div>
     </div>
 
+         <p class="text-white absolute bg-transparent mt-[185px] text-[10px] ml-3" v-if="emailError">{{ emailError }}</p>
+         <p class="text-white absolute bg-transparent mt-[255px] text-[10px] ml-3" v-if="passwordError">{{ passwordError }}</p>
 
 
 
@@ -53,7 +56,7 @@
 
             <button
                 class="px-2.5 border-none rounded-full bg-red-500 text-white cursor-pointer w-3/5 mx-auto transform transition-transform duration-300 ease-in-out font-bold mt-10 pt-2 pb-2"
-                type="submit" @click="$router.push('/home')" >Register</button>
+                type="submit" v-on:click="registerUser" >Register</button>
         </form>
 
 
@@ -88,14 +91,77 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
     data() {
         return {
             isClicked: false,
+            password: '',
+            passwordError: '',
+            email: '',
+            emailError: '',
         };
     },
-};
+    methods: {
+
+        validatePassword() {
+            this.passwordError = '';
+            if (this.password.length < 6) {
+                this.passwordError = 'Le mot de passe doit contenir au moins 6 caractères.';
+            } else if (!/\d/.test(this.password)) {
+                this.passwordError = 'Le mot de passe doit contenir au moins un chiffre.';
+            } else if (!/[a-z]/.test(this.password)) {
+                this.passwordError = 'Le mot de passe doit contenir au moins une lettre minuscule.';
+            } else if (!/[A-Z]/.test(this.password)) {
+                this.passwordError = 'Le mot de passe doit contenir au moins une lettre majuscule.';
+            }
+        },
+
+        validateEmail() {
+            this.emailError = '';
+            const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!check.test(this.email.toLowerCase())) {
+                this.emailError = 'Veuillez entrer une adresse email valide.';
+            }
+        },
+
+
+         async registerUser() {
+            console.log('tes dans la fonction registerUser batard');
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const username = document.getElementById('username').value;
+
+         try {
+                const response = await axios.post('http://localhost:1337/api/auth/local/register', {
+                    username: username,
+                    email: email,
+                    password: password,
+                }, {
+                    timeout: 5000, // Augmente le délai d'attente à 5 secondes
+                    headers: {
+                        'Access-Control-Allow-Origin': '*', // Autorise les requêtes CORS de tous les domaines
+                    },
+                });
+
+                console.log(response.data);
+                this.$router.push('/login');
+            } catch (error) {
+                console.error(error);
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log('Error', error.message);
+                }
+                console.log(error.config);
+            }
+        },
+    }
+}
 
 </script>
 
