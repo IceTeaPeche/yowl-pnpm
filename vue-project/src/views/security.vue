30339b4b0faaf6163d42f4e83e427da69bce1f16
LouisonLongprez <146077027+LouisonLongprez@users.noreply.github.com>
2024-02-02 10:49:48 +0100

    secu bio

--- vue-project/src/views/security.vue
+++ vue-project/src/views/security.vue
@@ -43,7 +43,18 @@
                 </button>
     
                  <button class="mb-6" @click="$router.push('/pseudo')"> <div class="w-[374px] h-[84px] pl-[118px] py-2.5 bg-custom-gray rounded-[20px] justify-between items-center inline-flex ml-7 ">
-                    <div class="w-[108px] text-center text-white text-xl font-bold bg-transparent">pseudo</div>
+                    <div class="w-[108px] text-center text-white text-xl font-bold bg-transparent">Pseudo</div>
+                        <div class="w-[53px] h-[83px] p-2.5 bg-red-500 rounded-tr-[20px] rounded-br-[20px] justify-center items-center gap-[23.18px] flex">
+            
+                                <div class="bg-transparent">
+                                <img class="bg-transparent" src="../assets/Arrow_Left_MDright.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </button>
+
+                <button class="mb-6" @click="$router.push('/bio')"> <div class="w-[374px] h-[84px] pl-[118px] py-2.5 bg-custom-gray rounded-[20px] justify-between items-center inline-flex ml-7 ">
+                    <div class="w-[108px] text-center text-white text-xl font-bold bg-transparent">Bio</div>
                         <div class="w-[53px] h-[83px] p-2.5 bg-red-500 rounded-tr-[20px] rounded-br-[20px] justify-center items-center gap-[23.18px] flex">
             
                                 <div class="bg-transparent">
