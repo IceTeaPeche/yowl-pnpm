ce2f87eaf652a4fdbb962d9ef267cd042c399b80
LouisonLongprez <146077027+LouisonLongprez@users.noreply.github.com>
2024-02-02 13:58:01 +0100

    certi

---
+++ vue-project/src/views/certification.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex mt-8">
+
+<img class="ml-6 " src="../assets/Arrow_Left_MD.svg" alt=""
+   @click="$router.push('/parameters')">
+<h1 class="text-white text-[25px] ml-28 font-bold ">Certification</h1>
+
+</div>
+
+<div class="text-center ml-2.5 mr-2.5" style="margin-top: 100px;">
+
+<div class="text-white mb-3">
+ Are you a game creator? Do you want to stand out from the rest?
+ Then, submit a certification request to the following email address: indécertif@gmail.com.
+</div>
+
+</div>
+
+<img src="../assets/blob-verified.svg" alt="Certif Logo" style="width: 200px; height: 200px;" class="mt-4 mx-auto">
+
+</template>
